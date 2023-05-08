e888476ba8e07dfc07c185611bad43d5a6e99af2
Amit Biswas <amit.biswas@welldev.io>
2023-05-08 15:45:57 +0600

    Added: Basic components, Updated: tsconfig

--- the-course/src/app/app.module.ts
+++ the-course/src/app/app.module.ts
@@ -3,9 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from '@/app/app-routing.module';
 import { AppComponent } from '@/app/app.component';
+import { CourseCardComponent } from '@/components/course-card/course-card.component';
+import { ButtonComponent } from '@/components/button/button.component';
+import { CourseListComponent } from '@/components/course-list/course-list.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    CourseCardComponent,
+    ButtonComponent,
+    CourseListComponent,
+    NavigationComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
